f0e5161a23f701ee6ec7c4662ef0e52e64c56f95
vasim katiya <vasimkatiya97@gmail.com>
2026-08-04 09:48:19 +0530

    auth fixing2.1

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -7,6 +7,6 @@ const authRouter = Router();
 authRouter.post('/register',registerController);
 authRouter.post('/login',loginController);
 authRouter.post('/logout',logoutController);
-authRouter.get('/profile',authHandler,profileController);
+authRouter.get('/profile',profileController);
 
 module.exports = authRouter;

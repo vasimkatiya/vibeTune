d5ff47b8d3963d535c6598e0498b700ca8718ff8
vasim katiya <vasimkatiya97@gmail.com>
2026-08-04 07:42:39 +0530

    modify cors

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-  origin:"https://vibetune83.netlify.app",
+  origin:"https://vibe-tune-omega.vercel.app",
   credentials:true
 }));
 
